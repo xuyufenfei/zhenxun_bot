1065708318ca30d9b689bf49e1aaa408eba75b1e
xuyufenfei <133896005+xuyufenfei@users.noreply.github.com>
2023-12-26 23:30:45 +0800

    Update config.py

--- configs/config.py
+++ configs/config.py
@@ -22,11 +22,11 @@ NICKNAME: str = "小真寻"
 # 示例："bind": "postgres://user:password@127.0.0.1:5432/database"
 bind: str = ""  # 数据库连接链接
 sql_name: str = "postgres"
-user: str = ""  # 数据用户名
-password: str = ""  # 数据库密码
-address: str = ""  # 数据库地址
-port: str = ""  # 数据库端口
-database: str = ""  # 数据库名称
+user: str = "postgres"
+password: str = "zhenxun_bot"
+address: str = "127.0.0.1"
+port: str = "5432"
+database: str = "zhenxun_bot"
 
 # 代理，例如 "http://127.0.0.1:7890"
 # 如果是WLS 可以 f"http://{hostip}:7890" 使用寄主机的代理
